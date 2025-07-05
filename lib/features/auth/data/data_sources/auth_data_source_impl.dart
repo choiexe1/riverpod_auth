23fdae50822ff88ed558aa10a9043fab40f2a18d
Junsung Choi <choiexe1@gmail.com>
2025-07-05 14:21:50 +0900

    login api

--- lib/features/auth/data/data_sources/auth_data_source_impl.dart
+++ lib/features/auth/data/data_sources/auth_data_source_impl.dart
@@ -1,11 +1,13 @@
 import 'package:dio/dio.dart';
 import 'package:rpod_auth/core/constants.dart';
-import 'package:rpod_auth/features/auth/domain/data_sources/auth_data_source.dart';
 import 'package:retrofit/retrofit.dart';
 
 part 'auth_data_source_impl.g.dart';
 
 @RestApi(baseUrl: authApiBaseUrl)
-abstract class AuthDataSourceImpl implements AuthDataSource {
+abstract class AuthDataSourceImpl {
   factory AuthDataSourceImpl(Dio dio, {String? baseUrl}) = _AuthDataSourceImpl;
+
+  @POST('/login')
+  Future<String> login({String username, String password});
 }
