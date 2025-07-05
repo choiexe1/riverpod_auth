506c8299641c81de09c7662237eeb50361fdc238
Junsung Choi <choiexe1@gmail.com>
2025-07-05 14:19:23 +0900

    di

--- lib/injector.dart
+++ lib/injector.dart
@@ -1,5 +1,11 @@
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+import 'package:rpod_auth/features/auth/data/data_sources/auth_data_source_impl.dart';
+import 'package:rpod_auth/features/auth/domain/data_sources/auth_data_source.dart';
 
 final GetIt sl = GetIt.instance;
 
-void injection() {}
+void injection() {
+  sl.registerLazySingleton<Dio>(Dio.new);
+  sl.registerLazySingleton<AuthDataSource>(() => AuthDataSourceImpl(sl()));
+}
